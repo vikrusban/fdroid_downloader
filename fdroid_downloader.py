57a7df0aa70d1b3458558de6831041dba5f38685
vik <linux.shift47@gmail.com>
2022-02-13 02:57:57 +0500

    Fix get empty item

--- fdroid_downloader.py
+++ fdroid_downloader.py
@@ -16,24 +16,27 @@ with open(f) as file:
     array = [row.strip() for row in file]
 
 for i in array:
-    resp = requests.get(i)
-    resp.encoding = 'utf-8'
-    soup = BeautifulSoup(resp.text, 'html.parser')
+    if i == '':
+        print('Empty string to file')
+    else:
+        resp = requests.get(i)
+        resp.encoding = 'utf-8'
+        soup = BeautifulSoup(resp.text, 'html.parser')
 
-    links_with_text = []
-    for a in soup.find_all('a', attrs={'href': re.compile('^https://f-droid.org/repo/')}):
-        if a.text:
-            links_with_text.append(a['href'])
+        links_with_text = []
+        for a in soup.find_all('a', attrs={'href': re.compile('^https://f-droid.org/repo/')}):
+            if a.text:
+                links_with_text.append(a['href'])
 
-    file_for_url = urlparse(links_with_text[1])
-    pre_file = file_for_url.path[+1:]
-    file = re.sub(r'(_\d{1,254}.apk)', '.apk', pre_file)
-    file_check = Path(file_for_url.path[+1:])
+        file_for_url = urlparse(links_with_text[1])
+        pre_file = file_for_url.path[+1:]
+        file = re.sub(r'(_\d{1,254}.apk)', '.apk', pre_file)
+        file_check = Path(file_for_url.path[+1:])
 
-    download_file = open(r''+file+'','wb')
-    ufr = requests.get(''+links_with_text[1]+'')
-    download_file.write(ufr.content)
-    download_file.close()
-    print('Download: '+links_with_text[1])
+        download_file = open(r''+file+'','wb')
+        ufr = requests.get(''+links_with_text[1]+'')
+        download_file.write(ufr.content)
+        download_file.close()
+        print('Download: '+links_with_text[1])
 
 print('Done')
